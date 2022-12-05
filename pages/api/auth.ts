52704033b6e78f3a6ed1ad678945418425297043
DarumaDocker <DarumaDocker@gmail.com>
2022-12-05 14:56:36 +0800

    test upstash

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -1,13 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextRequest, NextResponse } from 'next/server';
+import { redis } from '@/lib/upstash';
 
-const hello = (req: NextRequest) => {
+const auth = async (req: NextRequest) => {
+
+  await redis.set('a', '0');
   return NextResponse.json({
     name: `Hello, from ${req.url} I'm now an Edge Function!`,
   });
 };
 
-export default hello;
+export default auth;
 
 export const config = {
   runtime: 'experimental-edge',
